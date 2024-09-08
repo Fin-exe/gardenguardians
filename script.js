4a845af9cabf081da808a5ea2064d4081b271b0c
awipat17 <tchenje42@gmail.com>
2024-09-08 21:19:21 +1000

    Adding to Javascript
    Added code for the drag and drop on the main page

--- script.js
+++ script.js
@@ -38,3 +38,54 @@ card.addEventListener("click", function (e) {
          }
     });
 });
+
+const icon = document.getElementsByClassName('top_right_icons')[0]; // Access the first element
+const target = document.getElementsByClassName('center_content')[0]; // Access the first element
+let initialX, initialY;
+
+icon.addEventListener('mousedown', (e) => {
+    initialX = e.clientX - icon.offsetLeft;
+    initialY = e.clientY - icon.offsetTop;
+
+    icon.src = 'img/watercan1.png'; // Change image to pouring can
+
+    document.addEventListener('mousemove', moveIcon);
+    document.addEventListener('mouseup', dropIcon);
+});
+
+function moveIcon(e) {
+    icon.style.left = `${e.clientX - initialX}px`;
+    icon.style.top = `${e.clientY - initialY}px`;
+}
+
+function dropIcon(e) {
+    document.removeEventListener('mousemove', moveIcon);
+    document.removeEventListener('mouseup', dropIcon);
+
+    const canRect = icon.getBoundingClientRect();
+    const targetRect = target.getBoundingClientRect();
+
+    if (
+        canRect.right > targetRect.left &&
+        canRect.left < targetRect.right &&
+        canRect.bottom > targetRect.top &&
+        canRect.top < targetRect.bottom
+    ) {
+        // Center the watering can over the target
+        const targetCenterX = targetRect.left + targetRect.width / 2;
+        const targetCenterY = targetRect.top + targetRect.height / 2;
+
+        const canHalfWidth = canRect.width / 2;
+        const canHalfHeight = canRect.height / 2;
+
+        icon.style.left = `${targetCenterX - canHalfWidth}px`;
+        icon.style.top = `${targetCenterY - canHalfHeight}px`;
+    }
+
+    /* Reset image and position
+    setTimeout(() => {
+        icon.src = 'img/watercan1.png'; // Change back to original image
+        icon.style.left = '0px';
+        icon.style.top = '0px';
+    }, 200); // Delay before resetting, allowing the watering action to be visible*/
+}
