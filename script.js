8c90d7af26cab805d95ba71336e291fe40197e2b
awipat17 <tchenje42@gmail.com>
2024-09-10 09:50:15 +1000

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,79 @@ function resetQuiz() {
         button.disabled = false; 
     });
 }
+
+const nativePlantsURL = 'https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/free-native-plants-species/records?limit=40' 
+const weatherURL = 'https://api.open-meteo.com/v1/forecast?latitude=-27.4679&longitude=153.0281&hourly=temperature_2m,apparent_temperature,rain,showers,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high&daily=temperature_2m_max&timezone=auto&models=bom_access_global'
+
+async function fetchData(URL) {
+        try {
+            const firstResponse = await fetch(URL);
+            const firstJSON = await firstResponse.json();
+            //Return the second key of the JSON containing the array needed
+            const Result = firstJSON
+            console.log(Result)
+        } catch (error) {
+            console.error('Couldnt fetch the data :(');
+        }
+
+    }
+function filterDay() {
+    const today = new Date();
+    const year = today.getFullYear();  
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
+    const day = String(today.getDate()).padStart(2, '0'); // Pad single-digit day with a leading zero
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate
+}
+
+async function cardProperties() {
+    const data = await fetchData(nativePlantsURL)
+    const plantData = data.results
+    
+    const descriptions = plantData.map(plant => plant.description_and_growing_requirements)
+    
+    const indexAndSpecies = plantData.map(plant => ({
+        species: plant.species,
+        descript: plant.description_and_growing_requirements,
+        attract: plant.attracts
+    }));
+    console.log(indexAndSpecies)
+}
+
+async function weatherProperties() {
+    const data = await fetchData(weatherURL)
+    const weatherData = data.hourly
+
+    weatherData
+}
+
+async function saveCache(filename, data) {
+    try {
+        // Ensure filename includes file extension if needed
+        const filePath = `${filename}`;
+        
+        // Write data to the file, pretty-print JSON
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
+        
+    } catch (error) {
+        console.error("Error saving cache:", error);
+    }
+}
+
+async function readCache(filename) {
+    if (enableReadCache) {
+        try {
+            const data = await fs.readFile(filename, 'utf-8'); // Read file content as a string
+            
+            return JSON.parse(data); // Parse the JSON string back into an object
+            
+        } catch (error) {
+            console.error("Error reading cache:", error);
+            return null;
+        }
+    }
+    return null;
+}
+
