941aa82596c19717d44c89d88af40c9ac2e76ee4
emi238 <emilyho238@outlook.com>
2024-09-15 22:35:43 +1000

    Em - NAV BAR

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// NAV BAR
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}  
+
 document.addEventListener('DOMContentLoaded', function () {
     //please put all event listeners here
     handlePlantGrowth();
@@ -30,6 +60,7 @@ function handlePlantGrowth() {
     });
 }
 
+// FLIPCARD 
 function handleCardFlip() {
     const card = document.querySelector(".card__inner");
 
@@ -138,7 +169,7 @@ function setupDragDrop() {
     });
 }
 
-
+// WEATHER 
 window.onload = function() {
     setupDragDrop();
 };
