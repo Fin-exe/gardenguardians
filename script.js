880a5f0d97307e0dd37114575e8a8a6030eb17bc
Fin-exe <67260653+Fin-exe@users.noreply.github.com>
2024-10-15 21:58:39 +1000

    filter fix

--- script.js
+++ script.js
@@ -823,15 +823,16 @@ async function initializeDataFetch() {
     weatherElement.style.backgroundImage = `url("img/${weatherImg}.gif")`;
 }
 
-filterSelection("all")
+// SEED PAGE FILTERING
 
-//SEED PAGE FILTERING
+// Initial call to display all items
+filterSelection("all");
 
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  // Loop through all elements and hide those that don't match the filter
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
@@ -840,39 +841,39 @@ function filterSelection(c) {
 
 // Show filtered elements
 function w3AddClass(element, name) {
-  var i, arr1, arr2;
+  var arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+  arr2.forEach(function(n) {
+    if (arr1.indexOf(n) == -1) {
+      arr1.push(n);
     }
-  }
+  });
+  element.className = arr1.join(" ");
 }
 
 // Hide elements that are not selected
 function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
+  var arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
+  arr1 = arr1.filter(function(n) {
+    return arr2.indexOf(n) === -1;
+  });
   element.className = arr1.join(" ");
 }
 
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
+Array.from(btns).forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    var current = btnContainer.getElementsByClassName("active");
+    if (current.length > 0) current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
-}
+});
+
 
 function flickrAPI(){
     const baseFlickr = ""
