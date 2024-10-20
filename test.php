ee8ac576796533bcb468371e227b50583121de03
Vyshnav Madhuchandran <130115805+vmadh2@users.noreply.github.com>
2024-10-20 10:35:49 +1000

    reverting to 19/10

--- test.php
+++ test.php
@@ -1,20 +1,20 @@
 <?php
-// Flickr API configuration
-$api_key = 'eecd5014501ebeeec6d473f4c8311e32';
+// // Flickr API configuration
+// $api_key = 'eecd5014501ebeeec6d473f4c8311e32';
 
-$url = 'https://www.flickr.com/services/feeds/photos_public.gne?format=php_serial&tags=cutleafdaisy&per_page=1';
+// $url = 'https://www.flickr.com/services/feeds/photos_public.gne?format=php_serial&tags=cutleafdaisy&per_page=1';
 
-$data = file_get_contents($url);
-print_r($data);
+// $data = file_get_contents($url);
+// print_r($data);
 
-exit;
+// exit;
 
-$photos = $data->photos->photo;
-foreach ($photos as $photo) {
-    $url = 'https://farm'.$photo->farm.'.staticflickr.com/'.$photo->server.'/'.$photo->id.'_'.$photo->secret.'.jpg';
-    echo '<img src="'.$url. '">';
-    print_r($photo);
-    break;
-}
+// $photos = $data->photos->photo;
+// foreach ($photos as $photo) {
+//     $url = 'https://farm'.$photo->farm.'.staticflickr.com/'.$photo->server.'/'.$photo->id.'_'.$photo->secret.'.jpg';
+//     echo '<img src="'.$url. '">';
+//     print_r($photo);
+//     break;
+// }
 
 ?>
